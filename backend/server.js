1a3955df4b5480991c74c062204c9d2c585f0882
Krisztian Bui <buikrisz00@gmail.com>
2022-04-06 14:52:09 +0200

    base

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const frontend = path.join(`${__dirname}/../frontend`);
+const port = 9000;
+
+app.get("/", (req, res) => {
+    res.sendFile(`${frontend}/index.html`);
+})
+
+app.get("/image-list", (req, res) => {
+    fs.readFile(`${frontend}/data.json`, (error, data) => {
+        if (error) {
+            console.log(error);
+            res.send("Error reading file");
+        } else {
+            const images = JSON.parse(data);
+            res.send(images);
+        }
+    })
+})
+
+
+
+
+app.use("/public", express.static(`${frontend}/public`));
+
+app.listen(port, () => {
+    console.log(`http://127.0.0.1:${port}`);
+})
